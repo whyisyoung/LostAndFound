7f3ba9523893ef9a38c2ec4840ba110b23383f1a
linus-young <ylm19921123@msn.cn>
2014-05-10 21:38:52 +0800

    home modified by fh

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -2,8 +2,8 @@ module LayoutHelper
 
   def bootstrap_version
     # Choose the Bootstrap version you would like to use from the CDN
-    "3.0.3"
-    #"2.3.2"
+    #"3.0.3"
+    "2.3.2"
   end
 
 end
